8b0670087c1821468c9702ff0de4ee725d57ef82
Balaji-Kandavel <balaji.kandavel@coxautoinc.com>
2020-10-16 09:46:14 -0400

    Tests and formattting

--- src/main/java/com/adp/coding/challenge/changemachine/controller/ChangeMachineController.java
+++ src/main/java/com/adp/coding/challenge/changemachine/controller/ChangeMachineController.java
@@ -2,6 +2,8 @@ package com.adp.coding.challenge.changemachine.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,7 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.adp.coding.challenge.changemachine.model.Coin;
@@ -20,11 +21,12 @@ import com.adp.coding.challenge.changemachine.service.ChangeMachineService;
 @RestController
 public class ChangeMachineController {
 
+	Logger logger = LoggerFactory.getLogger(ChangeMachineController.class);
+
 	@Autowired
 	ChangeMachineService changeMachineService;
 
 	@GetMapping(value = "/coins", produces = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody
 	public String getAvailableCoins() {
 
 		return changeMachineService.getAvailableCoins();
@@ -37,12 +39,11 @@ public class ChangeMachineController {
 	}
 
 	@GetMapping(value = "/change/{cash}", produces = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody
 	public ResponseEntity<String> getChange(@PathVariable Double cash,
 			@RequestParam(name = "allowLeastCoins") Boolean allowLeastCoins) {
 		try {
 			String response = changeMachineService.getChange(cash, allowLeastCoins);
-
+			logger.info(response);
 			return new ResponseEntity<>(response, HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
